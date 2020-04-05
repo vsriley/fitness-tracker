07fa6efa3beddea1ba9cd56543bfff6013027f17
Valerie Riley <valeriesriley@gmail.com>
2020-04-05 14:25:29 -0600

    fixed server and routing issues

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,14 @@
 const path = require("path");
-const router = require("express");
+const router = require("express").Router();
 
 // routes to exercise page
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercises.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 // routes to stats page
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts");
 
