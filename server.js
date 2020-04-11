53f7856fa455d36856521e9d10eb31225dfdef4f
Valerie Riley <valeriesriley@gmail.com>
2020-04-11 00:41:53 -0600

    heroku setup

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose connection
+// mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
